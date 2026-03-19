bb92505ad032b735b94b3b57578eb6eeac3727c1
discipuloJL <lucasenglish54@gmail.com>
2026-03-19 15:18:19 -0300

    alterações gerais em perfil e nome de usuário

--- script.js
+++ script.js
@@ -7,20 +7,20 @@ function toggleMode() {
 
   // substituir a imagem
   if (html.classList.contains("dark-mode")) {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/gon2.png")
     img.setAttribute(
       "alt",
-      "Foto de José Lucas, pregando feliz de palitó azul escuro, camisa interna azul e gravata cor dourada sobre um púlpito de vidro com um microfone na mão esquerda numa igreja com um letreiro atrás na parede amarelada escrito 'Jesus vem' no estilo ghibli",
+      "Perfil de JL Code, personagem Gon de Hunter x Hunter, perspectiva diagonal",
     )
   }
 
   // se tiver sem light-mode, manter a imagem normal;
   else {
     // se tiver light-mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/gon1.png")
     img.setAttribute(
       "alt",
-      "Foto diagonal de José Lucas, pregando feliz de palitó escuro, camisa interna azul marinho sobre um púlpito de madeira com um microfone na mão direita e apontando a esquerda pra sua frente numa igreja com um letreiro atrás na parede azul claro escrito 'JESUS VEM' no estilo ghibli",
+      "Perfil de JL Code, personagem Gon de Hunter x Hunter, perspectiva frontal",
     )
   }
 }
